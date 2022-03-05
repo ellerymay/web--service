3cb004717114237644e74f9c9bc3e4d62b16dab5
ellerymay <emcsarquilla@gmail.com>
2022-03-05 09:48:44 -0800

    added photos

---
+++ data/cafe-seattle.js
@@ -0,0 +1,50 @@
+{
+	"title": "Cafe's in Seattle",
+	"cafes":[
+        {
+				"Name":"Hello Em",
+				"Location":"International District",
+				"Image":"EggCloudAmericano.jpg"
+			  },
+        {
+				"Name":"Cafe Hagen",
+				"Location":"South Lake Union",
+				"Image":"cafe hagen.jpg"
+			  },
+        {
+				"Name":"Ben Paris",
+				"Location":"Pike Place",
+				"Image":"benparis.jpg"
+			  },
+        {
+				"Name":"Macrina Bakery",
+				"Location":"Madron",
+				"Image":"macarinabakery.jpg"
+			  },
+        {
+				"Name":"Ada tech cafe",
+				"Location":"Madron",
+				"Image":"adas.jpg"
+			  },
+        {
+				"Name":"Cafe Umbria",
+				"Location":"Pioneer",
+				"Image":"cafeumbria.jpg"
+			  },
+        {
+				"Name":"Sabine Cafe",
+				"Location":"Capitol Hill",
+				"Image":"sabine.jpg"
+			  },
+        {
+				"Name":"London Plane",
+				"Location":"Pioneer Square",
+				"Image":"londonplane.jpg"
+			  },
+        {
+				"Name":"Olmstead",
+				"Location":"Capitol hill",
+				"Image":"olm.jpg"
+			  }
+			]
+}
